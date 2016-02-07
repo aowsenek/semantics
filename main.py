858bcac2920c8da7d2e7c3403bf59c1903206540
Anya Owsenek <aowsenek@Atria.home>
2016-02-06 19:26:22 -0500

    mainsubfunction

---
+++ main.py
@@ -0,0 +1,16 @@
+
+def inputparagraph():
+    st = input("Paragraph: ")
+    words = st.split()
+    clasues = st.split(",.")
+    phrases = []
+    for i in words.length-2:
+        phrases.append([words[i],words[i+1],words[i+2])
+    return words, clauses, phrases
+
+
+
+
+def main():
+   words, clauses, phrases = inputparagraph()
+   
